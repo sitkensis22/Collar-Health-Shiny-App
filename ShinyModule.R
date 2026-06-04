04bc690e44571aa01f923e2ed4bc87ea3d79b59e
Daniel Eacker <102631235+sitkensis22@users.noreply.github.com>
2026-06-04 13:25:14 -0600

    Update ShinyModule.R
    Fixed error with dataset in shapefile download.

--- ShinyModule.R
+++ ShinyModule.R
@@ -1138,7 +1138,7 @@ if(input$notification_type == "voltage" & unique(data_individual()$nAlerts) > 0)
         write.csv(ind_table_data(), file, row.names = FALSE)  
       }else
       if(input$download_select == "Shapefile"){
-        suppressWarnings(write_sf(data, dsn = file, layer = file, driver = "ESRI Shapefile", append = FALSE)) 
+        suppressWarnings(write_sf(data_individual(), dsn = file, layer = file, driver = "ESRI Shapefile", append = FALSE)) 
       }else  
       if(input$download_select == "Report"){
        # Parameters to pass to the Rmd
